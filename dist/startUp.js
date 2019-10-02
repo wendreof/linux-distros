5d3005d905d4453d6ea15c27ccb2f434b36c347c
Wendreo <wendreolf@gmail.com>
2019-10-01 22:21:02 -0300

    Adding routes

--- dist/startUp.js
+++ dist/startUp.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
 const db_1 = require("./infra/db");
 const bodyParser = require("body-parser");
+const linuxControllers_1 = require("./controllers/linuxControllers");
 class StartUp {
     constructor() {
         this.app = express();
@@ -18,11 +19,21 @@ class StartUp {
     routes() {
         this.app.route("/").get((req, res) => {
             res.send({
-                author: "Wendreo L. Fernandes",
+                author: "@wendreof",
                 updatedAt: "09/16/19",
                 version: "0.0.0.1"
             });
         });
+        // ..::get::..
+        this.app.route("/api/v1/distros").get(linuxControllers_1.default.get);
+        // ..::getById::..
+        this.app.route("/api/v1/distros/:id").get(linuxControllers_1.default.getById);
+        // ..::create::..
+        this.app.route("api/v1/distros").post(linuxControllers_1.default.create);
+        // ..::update::..
+        this.app.route("api/v1/distros/:id").put(linuxControllers_1.default.update);
+        // ..::delete::..
+        this.app.route("api/v1/distros/:id").delete(linuxControllers_1.default.delete);
     }
 }
 exports.default = new StartUp();
--- startUp.ts
+++ startUp.ts
@@ -4,6 +4,8 @@ import Database from "./infra/db";
 
 import * as bodyParser from "body-parser";
 
+import LinuxController from "./controllers/linuxControllers";
+
 class StartUp {
   public app: express.Application;
   private _db: Database;
@@ -25,11 +27,22 @@ class StartUp {
   routes() {
     this.app.route("/").get((req, res) => {
       res.send({
-        author: "Wendreo L. Fernandes",
+        author: "@wendreof",
         updatedAt: "09/16/19",
         version: "0.0.0.1"
       });
     });
+
+    // ..::get::..
+    this.app.route("/api/v1/distros").get(LinuxController.get);
+    // ..::getById::..
+    this.app.route("/api/v1/distros/:id").get(LinuxController.getById);
+    // ..::create::..
+    this.app.route("api/v1/distros").post(LinuxController.create);
+    // ..::update::..
+    this.app.route("api/v1/distros/:id").put(LinuxController.update);
+    // ..::delete::..
+    this.app.route("api/v1/distros/:id").delete(LinuxController.delete);
   }
 }
 
