b0b33c93d1b646956785e855d79fbcffe44ac2f7
Wendreo <wendreolf@gmail.com>
2019-10-01 21:31:18 -0300

    Creating controller

---
+++ controllers/linuxControllers.ts
@@ -0,0 +1,15 @@
+import LinuxService from "../services/linuxServices";
+
+class LinuxController {
+  get(req, res) {}
+
+  getById(req, res) {}
+
+  create(req, res) {}
+
+  update(req, res) {}
+
+  delete(req, res) {}
+}
+
+export default new LinuxController();
---
+++ dist/controllers/linuxControllers.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class LinuxController {
+    get(req, res) { }
+    getById(req, res) { }
+    create(req, res) { }
+    update(req, res) { }
+    delete(req, res) { }
+}
+exports.default = new LinuxController();
